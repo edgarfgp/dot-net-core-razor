129af1c38c1a6ba8e9564394bd2f355be3540f05
Edgar Fabian Gonzalez Peña <edgargonzalez.developer@gmail.com>
2018-03-24 20:45:13 +0000

    Services has ViewModel has been added

---
+++ OrderingApplication/Services/OrderService.cs
@@ -0,0 +1,31 @@
+﻿using OrderingApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderingApplication.Services
+{
+    public interface IOrder
+    {
+        List<Order> GetOrders();
+    }
+
+    public class OrderService : IOrder
+    {
+        public List<Order> GetOrders()
+        {
+            return new List<Order>()
+            {
+                new Order() { Name = "Donec id elit non", LastUpdated = DateTime.Now.AddDays(-2), Description = "Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit." },
+                new Order() { Name = "Maecenas sed diam eget risus", LastUpdated = DateTime.Now.AddDays(-3), Description = "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit." },
+                new Order() { Name = "Maecenas sed diam eget risus", LastUpdated = DateTime.Now.AddDays(-8), Description = "Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit." },
+                new Order() { Name = "Donec id elit non", LastUpdated = DateTime.Now.AddDays(-2), Description = "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit." },
+                new Order() { Name = "Maecenas sed diam eget risus", LastUpdated = DateTime.Now.AddDays(-5), Description = "Maecenas sed diam eget risus varius blandit. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit." },
+                new Order() { Name = "Donec id elit non", LastUpdated = DateTime.Now.AddDays(-15), Description = "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit." }
+            };
+
+
+        }
+    }
+}
---
+++ OrderingApplication/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using OrderingApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderingApplication.ViewModels
+{
+    public class HomeViewModel
+    {
+        public string Title { get; set; }
+        public List<Order> Orders { get; set; }
+    }
+}
