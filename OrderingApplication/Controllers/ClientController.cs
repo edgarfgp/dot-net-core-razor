566dbfac6a00dba2b05676f7682596957900a7a0
Edgar Fabian Gonzalez Peña <edgargonzalez.developer@gmail.com>
2018-03-24 20:44:20 +0000

    Client Controller ha been added

---
+++ OrderingApplication/Controllers/ClientController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OrderingApplication.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OrderingApplication.Controllers
+{
+    public class ClientController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult NewClient()
+        {
+            var newClient = new Client();
+            newClient.EmailPromos = new List<EmailPromo>()
+            {
+                new EmailPromo() { Id = 1, Title = "Weekly Deal", IsSubscribed = false },
+                new EmailPromo() { Id = 2, Title = "Holiday Promos", IsSubscribed = false },
+                new EmailPromo() { Id = 3, Title = "Company Newsletter", IsSubscribed = false },
+                new EmailPromo() { Id = 4, Title = "New Products", IsSubscribed = false }
+            };
+
+            return View(newClient);
+        }
+
+        [HttpPost]
+        public IActionResult NewClient(Client newClient)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View(newClient);
+            }
+        }
+
+        public IActionResult VerifyEmail(string email)
+        {
+            var emailList = new List<string>()
+            {
+                "test@example.com",
+                "hello@example.com",
+                "world@example.com"
+            };
+
+            if (emailList.Contains(email))
+            {
+                return Json("Email is already taken");
+            }
+
+            return Json(true);
+        }
+    }
+}
---
+++ OrderingApplication/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderingApplication.Services;
+using OrderingApplication.ViewModels;
+
+namespace OrderingApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private OrderService _orderService;
+
+        public HomeController(OrderService orderService)
+        {
+            _orderService = orderService;
+        }
+        public IActionResult Index()
+        {
+            var orders = _orderService.GetOrders();
+            var homeViewModel = new HomeViewModel
+            {
+                Title = "Orders",
+                Orders = orders
+
+            };
+            return View(homeViewModel);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
+
